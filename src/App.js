7007ea3a3e42ce89745019aedf92d78e8b1ea3b7
Bibek-2022 <71386018+Bibek-2022@users.noreply.github.com>
2022-06-20 22:21:38 +1000

    Installed all the dependencies for the project and created the folder structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FiSettings } from "react-icons/fi";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import "./App.css";
 export const App = () => {
-  return <div>App</div>;
+  const activeMenu = true;
+  return (
+    <div>
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+            <TooltipComponent content="Settings" position="top">
+              <button
+                type="button"
+                className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-grey text-white"
+                style={{ background: "blur", borderRadius: "50%" }}
+              >
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+              Sidebar
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">Sidebar w-0</div>
+          )}
+
+          <div
+            className={
+              activeMenu
+                ? "dark:bg-main-bg bg-main-bg min-h-screen md:ml-72 w-full"
+                : "ark:bg-main-bg bg-main-bg min-h-screen w-full flex-2"
+            }
+          ></div>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
