f4f9960186b582ec42993dde4912a1c9af93f2ab
Bibek-2022 <71386018+Bibek-2022@users.noreply.github.com>
2022-06-23 23:04:22 +1000

    Ecommerce Page

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import { BsCurrencyDollar } from "react-icons/bs";
+import { GoPrimitiveDot } from "react-icons/go";
+import { IoIosMore } from "react-icons/io";
+import { DropDownListComponent } from "@syncfusion/ej2-react-dropdowns";
 
+import { Stacked, Pie, Button, LineChart, SparkLine } from "../components";
+import {
+  earningData,
+  medicalproBranding,
+  recentTransactions,
+  weeklyStats,
+  dropdownData,
+  SparklineAreaData,
+  ecomPieChartData,
+} from "../data/dummy";
+import { useStateContext } from "../contexts/ContextProvider";
+import product9 from "../data/product9.jpg";
 const Ecommerce = () => {
-  return <div>Ecommerce</div>;
+  return (
+    <div className="mt-12">
+      <div className="flex flex-wrap lg:flex-nowrap justify-center">
+        <div className="bg-white dark:text-grey-200 dark:secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
+          <div className="flex justify-between items-center">
+            <div>
+              <p className="font-bold text-gray-400">Earnings</p>
+              <p className="text-2xl">$63,448.78</p>
+              <button
+                type="button"
+                style={{ backgroundColor: currentColor }}
+                className="text-2xl opacity-0.9 text-white hover:drop-shadow-xl rounded-full  p-4"
+              >
+                <BsCurrencyDollar />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Ecommerce;
