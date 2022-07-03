ecfe9da886ef8103103462447109da5d0e11906c
Bibek-2022 <71386018+Bibek-2022@users.noreply.github.com>
2022-07-03 21:26:50 +1000

    Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 
-export const Header = () => {
-  return <div>Header</div>;
-};
+export const Header = ({ category, title }) => (
+  <div className=" mb-10">
+    <p className="text-lg text-gray-400">{category}</p>
+    <p className="text-3xl font-extrabold tracking-tight text-slate-900">
+      {title}
+    </p>
+  </div>
+);
